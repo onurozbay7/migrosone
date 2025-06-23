27b3147558bcd91cdd2791223a2f196d5f6b36e6
Onur Özbay <ayon1524@gmail.com>
2025-06-23 16:51:40 +0300

    add unit test dependencies & update swagger doc

--- src/main/java/com/migrosone/controller/dto/CourierLocationRequest.java
+++ src/main/java/com/migrosone/controller/dto/CourierLocationRequest.java
@@ -1,5 +1,6 @@
 package com.migrosone.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -8,17 +9,22 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 @Getter
 @Setter
+@Schema(description = "Courier location request payload")
 public class CourierLocationRequest {
 
     @NotBlank
+    @Schema(description = "Unique ID of the courier", example = "courier_123")
     private String courierId;
 
     @NotNull
+    @Schema(description = "Latitude value", example = "40.9923")
     private Double lat;
 
     @NotNull
+    @Schema(description = "Longitude value", example = "29.1244")
     private Double lng;
 
     @NotNull
+    @Schema(description = "Location timestamp", example = "2024-06-20T14:33:00")
     private LocalDateTime timestamp;
 }
